82c44448a4a5b580b83f28c9d3390fce98f7207c
soleilyasmina <mounlasolomon@gmail.com>
2020-09-29 15:41:49 -0400

    finished app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
 
-  console.log(banana);
+  const kyle = 1000;
 
   const newProduct = {
     name: "rope",
@@ -18,10 +18,35 @@ function App() {
     price: "priceless",
   };
 
+  const handleSubmit = (event) => {
+    // page doesn't reload
+    event.preventDefault();
+    // i have to gather the product's information into an object
+    // property shorthand (i.e. name: name vs. JUST name)
+    const submittedProduct = {
+      name: name, // this can also be done with just name,
+      price: price,
+      description: description,
+      kyle,
+    };
+    // i want to put this new product into the products state
+    setProducts([...products, submittedProduct]);
+    // clear out the form
+    setName("");
+    setPrice("");
+    setDescription("");
+  };
+
   return (
     <div className="App">
       <h1>The Bizarre Bazaar</h1>
-      <form>
+      <div>
+        <h2>Preview our new item!</h2>
+        <h3>Name: {name}</h3>
+        <h4>Price: {price}</h4>
+        <h5>Description: {description}</h5>
+      </div>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input
           name="name"
@@ -29,6 +54,21 @@ function App() {
           value={name}
           onChange={(event) => setName(event.target.value)}
         />
+        <label htmlFor="price">Price:</label>
+        <input
+          name="price"
+          type="number"
+          value={price}
+          onChange={(event) => setPrice(event.target.value)}
+        />
+        <label htmlFor="description">Description:</label>
+        <textarea
+          name="description"
+          type="text"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <button type="submit">*BURP*</button>
       </form>
 
       <button onClick={() => setProducts([...products, newProduct])}>
